adeee25111aef5543a0df4175c1182dbe5cf708e
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 15:31:34 -0700

    add jquery to gender comments

--- app/assets/javascripts/ideas_index.js
+++ app/assets/javascripts/ideas_index.js
@@ -9,3 +9,11 @@ function loadIdeas() {
       $.each(ideas, function(index, idea) { renderIdea(idea) });
     });
 }
+
+function renderIdea(idea) {
+  $('#ideas-container')
+    .append("<div class='idea-container'>"
+           + "<h1>" + idea.title + "</h1>"
+           + "<p>" + idea.body + "<p>"
+           +"</div>")
+}
