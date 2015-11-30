8f5041a6b7674cb28620ae5599f0b689af460506
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 14:22:46 -0700

    add presence validation for title

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -5,6 +5,12 @@ describe Idea do
     expect(new_idea).to be_valid
   end
 
+  it 'is invalid without title' do
+    idea = new_idea
+    idea.title = nil
+    expect(idea).to_not be_valid
+  end
+
 
   def new_idea
     Idea.new(title: "Hi", body: "body", quality: 1)
