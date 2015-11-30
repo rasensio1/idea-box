3ecc31662d826c183003cb40595211d07119ea8e
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 14:21:22 -0700

    add simple validation test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe Idea do
-  it 'is true' do
-    assert true
+  it 'is valid' do
+    expect(new_idea).to be_valid
+  end
+
+
+  def new_idea
+    Idea.new(title: "Hi", body: "body", quality: 1)
   end
 end
