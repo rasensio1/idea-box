be51f66dabe0e917ca4f214e8da307f5ece2245f
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 17:57:15 -0700

    renders json for creating new idea

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -6,11 +6,13 @@ class Api::V1::IdeasController < ApplicationController
   def create
     new_idea = Idea.new(idea_params)
     if new_idea.save
+      render json: {text: "Saved new idea!"}
+    else
+      render json: {text: "Idea not saved!"}
     end
   end
 
   def idea_params
-    byebug
     params.require(:idea).permit(:title, :body)
   end
 end
