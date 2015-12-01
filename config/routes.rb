2fdf326dd4a69e354a07513272b27cb5d1780df1
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 17:43:48 -0700

    add route for create idea

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      resources 'ideas', only: [:index], defaults: {format: :json}
+      resources 'ideas', only: [:index, :create], defaults: {format: :json}
     end
   end
 end
