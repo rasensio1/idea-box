e436f2d45a48c029c8fca5b9217e9ad1d5beb712
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-30 17:55:48 -0700

    change test for clicking on div

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::IdeasController < ApplicationController
   def index
     render json: Idea.order(created_at: :desc)
   end
+
+  def create
+    new_idea = Idea.new(idea_params)
+    if new_idea.save
+    end
+  end
+
+  def idea_params
+    byebug
+    params.require(:idea).permit(:title, :body)
+  end
 end
--- spec/features/ideas_index_spec.rb
+++ spec/features/ideas_index_spec.rb
@@ -24,7 +24,7 @@ RSpec.feature 'the app' do
       page.fill_in 'idea_body',
         :with => 'my body'
 
-      click_on "Submit"
+      find("#submit-button").click
 
       expect(page).to have_content("my title")
       expect(page).to have_content("my body")
