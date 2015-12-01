22853e3d9031c48eb43591f0a729eb64f8f98f42
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-01 16:44:21 -0700

    add function to get and send form for editing an idea

--- app/assets/javascripts/edit_idea.js
+++ app/assets/javascripts/edit_idea.js
@@ -11,9 +11,28 @@ function renderEditForm(node) {
   $(node).parent().html(
       "<input type='text' name='idea[title]' class='edit_idea_title' id='" +id+"'>"
       + "<input type='text' name='idea[body]' class='edit_idea_body' id='"+id+"'>"
-      + "<div class='ui button edit_idea_button' id='"+id+ "'><p>Submit</p></div>"
+      + "<div class='ui button edit_idea_submit' id='"+id+ "'><p>Submit</p></div>"
       )
   $("#"+id+".edit_idea_title").val(title)
   $("#"+id+".edit_idea_body").val(body)
+  prepareEditSubmit();
+};
 
+function prepareEditSubmit() {
+  $(".edit_idea_submit").click(function() {
+    var title = $(this).siblings('.edit_idea_title').val()
+    var body = $(this).siblings('.edit_idea_body').val()
+    var id = $(this).attr('id')
+    $.ajax({
+       method: "PATCH",
+       url: "api/v1/ideas/" + ideaId,
+       data: { id: id,
+               title: title
+               body: body},
+       success: function() {
+         clearIdeas();
+         loadIdeas();
+       }
+    });
+  });
 };
